3799fd328dde109cfcdaaceceb59ee2effb09a66
surwase <pratap7684@gmail.com>
2017-12-01 13:57:17 +0530

    frist project git commit

---
+++ server.js
@@ -0,0 +1,31 @@
+
+const express = require('express');
+var app = express();
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrent', () => {
+    return new Date().getFullYear();
+});
+
+app.get('/', (req, res) => {
+		res.render('home.hbs', {
+		pageTitle: 'Home Pages',
+		currentYear: new Date().getFullYear()
+	});
+	});
+
+
+app.get('/About', (req, res) => {
+	//res.send('<h2>Hellow We are in About Page</h2>');
+	res.render('about.hbs', {
+		pageTitle: 'About Pages of',
+		currentYear: new Date().getFullYear()
+}); 
+});
+app.listen(3000, () => {
+	console.log('Server is started on 3000');
+});
